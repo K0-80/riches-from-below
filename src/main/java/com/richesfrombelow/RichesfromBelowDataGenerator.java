20c1ef25b1a27738170978ac06d32ba0012671e3
K0 <K080629@outlook.com>
2025-08-04 20:55:05 -0400

    first

---
+++ src/main/java/com/richesfrombelow/RichesfromBelowDataGenerator.java
@@ -0,0 +1,11 @@
+package com.richesfrombelow;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RichesfromBelowDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
